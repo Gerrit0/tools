e4ae7a6b926f9ea455c1ce798eb244c5e4d19c79
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2019-10-02 16:25:44 -0600

    Better graph builder

--- src/dfa_to_graph.ts
+++ src/dfa_to_graph.ts
@@ -1,47 +1,57 @@
+// This is reversed from the parse result used in the simulator.
+// Map<State, Map<State, Set<Inputs>>>
+function parseMachine (lines: string[]): Map<string, Map<string, Set<string>>> {
+  const result: Map<string, Map<string, Set<string>>> = new Map()
+
+  for (const line of lines) {
+    const [from, input, to] = line.split(',')
+    const transitions: Map<string, Set<string>> = result.get(from) || new Map()
+    const targets: Set<string> = transitions.get(to) || new Set()
+    targets.add(input)
+    transitions.set(to, targets)
+    result.set(from, transitions)
+  }
+
+  return result
+}
+
 export function dfaToGraph (text: string): string {
   const [first, ...rest] = text.split('\n')
   const out = ['digraph G {', '\trankdir=LR', '\tstart[shape=point style=invis]', '']
-  const out2 = ['']
+  const out2 = [`\tstart -> q_0`]
 
   const accept = first.replace(/^\{|\}$/g, '').split(',')
   const seen = new Set<string>()
-  let seenFirst = false
+  const machine = parseMachine(rest.filter(Boolean))
+  console.log(machine)
 
-  for (const line of rest.filter(Boolean)) {
-    const [from, what, to] = line.split(',')
-    if (!seenFirst) {
-      out2.push(`\tstart -> q_${from.replace(/`/g, 'eps')}`)
-      seenFirst = true
+  for (const [from, transitions] of machine) {
+    if (!seen.has(from)) {
+      seen.add(from)
+      const shape = accept.includes(from) ? 'doublecircle' : 'circle'
+      const label = `q<SUB>${from.replace(/`/g, 'eps')}</SUB>`
+      out.push(`\tq_${from.replace(/`/g, 'eps')}[shape=${shape} label=<${label}>]`)
     }
-    out2.push(
-      `\tq_${from.replace(/`/g, 'eps')} -> q_${to.replace(/`/g, 'eps')} [label=<${what.replace(
-        /`/g,
-        '&epsilon;'
-      )}>]`
-    )
-    seen.add(from)
-    seen.add(to)
-  }
 
-  for (const state of seen) {
-    if (accept.includes(state)) {
-      out.push(
-                `\tq_${state.replace(/`/g, 'eps')}[shape=doublecircle label=<q<SUB>${state.replace(
-                    /`/g,
-                    '&epsilon;'
-                )}</SUB>>]`
-            )
-    } else {
-      out.push(
-                `\tq_${state.replace(/`/g, 'eps')}[shape=circle label=<q<SUB>${state.replace(
-                    /`/g,
-                    '&epsilon;'
-                )}</SUB>>]`
-            )
+    for (const [target, inputs] of transitions) {
+      out2.push(
+        `\tq_${
+          from.replace(/`/g, 'eps')
+        } -> q_${
+          target.replace(/`/g, 'eps')
+        } [label=<${
+          [...inputs].join(',').replace(/`/g, '&epsilon;')
+        }>]`
+      )
+
+      if (!seen.has(target)) {
+        seen.add(target)
+        const shape = accept.includes(target) ? 'doublecircle' : 'circle'
+        const label = `q<SUB>${target.replace(/`/g, 'eps')}</SUB>`
+        out.push(`\tq_${target.replace(/`/g, 'eps')}[shape=${shape} label=<${label}>]`)
+      }
     }
   }
 
-  out2.push('}')
-
-  return out.concat(out2).join('\n')
+  return out.concat(out2, '}').join('\n')
 }
